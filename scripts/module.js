4b4df0fcd463c11dfe609942f955a4580173a3f9
CaptainBrando283 <113072385+CaptainBrando283@users.noreply.github.com>
2024-05-19 19:01:14 -0500

    Update module.js
    I do not know what I am doing so I am just coping code to see what it does

--- scripts/module.js
+++ scripts/module.js
@@ -1,4 +1,13 @@
 Hooks.once('init', async function() {
+Shadowdark.usageDieTypes = {
+    none: "bh2e.fields.labels.none",
+    d4:   "bh2e.dice.d4",
+    d6:   "bh2e.dice.d6",
+    d8:   "bh2e.dice.d8",
+    d10:  "bh2e.dice.d10",
+    d12:  "bh2e.dice.d12",
+    d20:  "bh2e.dice.d20"
+};
 
 });
 
